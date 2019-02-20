d66f1741618c217ee80b6120a4da557b620eb70e
carbuz2 <andrea_carbone85@hotmail.it>
2019-02-20 00:35:18 +0000

    Secure connection

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var http = require('http');
-//var https = require('https');
-var http2 = require('http2');
+var https = require('https');
+//var http2 = require('http2');
 var express = require('express');
 
 var app = express();
@@ -11,10 +11,10 @@ if (fs.existsSync( __dirname + '/../certificates/live/carbuzltd.com' )) {
   var privateKey  = fs.readFileSync(__dirname + '/../certificates/live/carbuzltd.com/privkey.pem', 'utf8');
   var certificate = fs.readFileSync(__dirname + '/../certificates/live/carbuzltd.com/cert.pem', 'utf8');
   var credentials = {key: privateKey, cert: certificate};
-  //var httpsServer = https.createServer(credentials, app);
+  var httpsServer = https.createServer(credentials, app);
   //httpsServer.listen(8443);
   var http2Server = http2.createSecureServer(credentials, app);
-  http2Server.listen(8080);
+  http2Server.listen(8443);
 } else {
   var httpServer = http.createServer(app);
   httpServer.listen(9223);
