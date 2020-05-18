b441253069a8dfdfafec9e84c45eeacf060c1b75
carbuz2 <andrea_carbone85@hotmail.it>
2020-05-18 15:42:16 +0100

    partial fix

--- dev/js/lib/custom.js
+++ dev/js/lib/custom.js
@@ -7,16 +7,25 @@
 
   anime.timeline({loop: true})
     .add({
-      targets: '.ml3 .letter',
-      opacity: [0,1],
-      easing: "easeInOutQuad",
-      duration: 2250,
-      delay: (el, i) => 150 * (i+1)
-    }).add({
-      targets: '.ml3',
-      opacity: 0,
-      duration: 1000,
-      easing: "easeOutExpo",
-      delay: 1000
-    });
+    targets: '.ml3 .letter',
+    opacity: [0,1],
+    easing: "easeInOutQuad",
+    duration: 2250,
+    delay: (el, i) => 150 * (i+1)
+  }).add({
+    targets: '.ml3',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+  
+  window.onkeyup = function(e) {
+    var event = e.which || e.keyCode || 0; // .which with fallback
+
+    if (event == 27) { // ESC Key
+        window.location.href = '#project'; // Navigate to URL
+    }
+  }
+
 })();
