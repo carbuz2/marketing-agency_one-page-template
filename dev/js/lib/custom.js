5bfd08ae2d1f91c3dfa85982c7d9c2edd2c452d4
carbuz2 <andrea_carbone85@hotmail.it>
2020-05-18 17:30:05 +0100

    fix location icon

--- dev/js/lib/custom.js
+++ dev/js/lib/custom.js
@@ -20,12 +20,5 @@
     delay: 1000
   });
   
-  window.onkeyup = function(e) {
-    var event = e.which || e.keyCode || 0; // .which with fallback
-
-    if (event == 27) { // ESC Key
-        window.location.href = '#project'; // Navigate to URL
-    }
-  }
 
 })();
